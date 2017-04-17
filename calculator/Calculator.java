70849a8759d4665ca616128842d3482630f86db9
Jacob Bender <jdbender64@gmail.com>
2017-04-16 23:07:52 -0700

    added menu loop and bare bones functionality for calculator methods

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -3,52 +3,83 @@ import java.util.*;
 public class Calculator {
    public static void main(String[] args) {
       System.out.println("Welcome to Calculator!\n");
-      System.out.println("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n");
-      
+      boolean menu = true;
       Scanner input = new Scanner(System.in);
       
-      System.out.print("What is your choice?: ");
-      int choice = input.nextInt();
+      while (menu) {
+         System.out.println("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Quit\n");
       
-      if (choice == 1) {
-         prompt();
-      }
-      else if (choice == 2) {
-         prompt();
-      }
-      else if (choice == 3) {
-         prompt();
-      }
-      else if (choice == 4) {
-         prompt();
-      }
-      else {
-         System.out.println("Error: Please try again");
+         System.out.print("What is your choice?: ");
+         
+         int choice = input.nextInt();
+         int x;
+         int y;
+         
+         if (choice == 1) {
+            x = first();
+            y = second();
+            int ans = Add(x, y);
+            System.out.println("The result of your calculation is " + ans + "\n");
+         }
+         else if (choice == 2) {
+            x = first();
+            y = second();
+            int ans = Subtract(x, y);
+            System.out.println("The result of your calculation is " + ans + "\n");
+         }
+         else if (choice == 3) {
+            x = first();
+            y = second();
+            int ans = Multiply(x, y);
+            System.out.println("The result of your calculation is " + ans + "\n");
+         }
+         else if (choice == 4) {
+            x = first();
+            y = second();
+            int ans = Divide(x, y);
+            System.out.println("The result of your calculation is " + ans + "\n");
+         }
+         else if (choice == 5) {
+            menu = false;
+         }
+         else {
+            System.out.println("Error: Please try again");
+         }
       }
    }
    
-   public int Add(int x, int y) {
+   public static int Add(int x, int y) {
       int z = x + y;
       return z;
    }
    
-   public int Subtract(int x, int y) {
+   public static int Subtract(int x, int y) {
       int z = x - y;
       return z;
    }
    
-   public int Multiply(int x, int y) {
+   public static int Multiply(int x, int y) {
       int z = x * y;
       return z;
    }
    
-   public int Divide(int x, int y) {
+   public static int Divide(int x, int y) {
       int z = x / y;
       return z;
    }
    
-   public static void prompt() {
-      System.out.println("First number: ");
-      System.out.println("Second number: ");
+   public static int first() {
+      Scanner input = new Scanner(System.in);
+      System.out.print("First number: ");
+      int x = input.nextInt();
+      return x;
+   }
+   
+   public static int second() {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Second number: ");
+      int y = input.nextInt();
+      System.out.println();
+      return y;
    }
 }
