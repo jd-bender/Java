45c2dce34ab2ad28f1d44d53f2af1af419e1b41b
Jacob Bender <jdbender64@gmail.com>
2017-04-16 22:42:34 -0700

    added methods for basic arithmetic

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -3,4 +3,24 @@ public class Calculator {
       System.out.println("Welcome to Calculator!");
       System.out.println("1. Add\n2. Subtract\n3. Multiply\n4. Divide");
    }
+   
+   public int Add(int x, int y) {
+      int z = x + y;
+      return z;
+   }
+   
+   public int Subtract(int x, int y) {
+      int z = x - y;
+      return z;
+   }
+   
+   public int Multiply(int x, int y) {
+      int z = x * y;
+      return z;
+   }
+   
+   public int Divide(int x, int y) {
+      int z = x / y;
+      return z;
+   }
 }
