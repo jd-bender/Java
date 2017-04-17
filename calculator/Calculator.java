4ce84ece3971b2ef282eaa35ba82558c38677dd6
Jacob Bender <jdbender64@gmail.com>
2017-04-16 22:28:48 -0700

    Initial push

---
+++ calculator/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+   public static void main(String[] args) {
+      System.out.println("Welcome to Calculator!");
+      System.out.println("1. Add\n2. Subtract\n3. Multiply\n4. Divide");
+   }
+}
